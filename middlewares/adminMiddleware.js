9058afdb9d9e9b454dd74d4cd78c9dd2e3b685bc
Saqlain Malik <saqlainmalik@maxifysolution.com>
2026-02-20 15:31:12 +0530

    by malik

--- middlewares/adminMiddleware.js
+++ middlewares/adminMiddleware.js
@@ -1,5 +1,5 @@
 const adminMiddleware = (req, res, next) => {
-  if (!req.user || req.user.role !== "admin") {
+  if (!req.user || req.user.role !== "admin"|| req.user.role !== "subadmin") {
     return res.status(403).json({
       success: false,
       message: "Admin access denied",
